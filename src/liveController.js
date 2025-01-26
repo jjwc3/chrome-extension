b0792538913546397d01c6947b45040fa5456aec
jjwc3 <155734556+jjwc3@users.noreply.github.com>
2025-01-26 20:04:37 +0900

    250126 License 추가 자동 UP 오류 수정

--- src/liveController.js
+++ src/liveController.js
@@ -1,12 +1,13 @@
 import { datetime } from './utils.mjs';
 import { getConfig, setConfig } from './config.mjs';
 
+/*
+라이브 관련 기능(도배 제외)
+ */
 (async () => {
-
     const enabled = await getConfig("twitch.controller.enabled");
     let checkLawEnabled = await getConfig("twitch.checkLawAlert.enabled");
     const upBj = (await getConfig("twitch.autoUp.custom")).split("\n");
-
     if (!enabled) return;
 
     let video;
@@ -24,6 +25,11 @@ import { getConfig, setConfig } from './config.mjs';
         video = document.getElementsByTagName("video")[0];
     }, 600);
 
+    /**
+     * confirm 창을 띄우고, 동의 시 true, 거부 시 false return
+     * checkLawEnabled == 0 일 경우 true return
+     * @returns {boolean}
+     */
     const checkLaw = () => {
         if (checkLawEnabled) {
             return confirm("설정한 화질대로 캡쳐됩니다. 최대화질로 설정 후 캡쳐해주세요.\n\n스트리머·저작권자의 동의 없이 녹화된 영상 및 캡쳐 이미지를 공유하는 경우, 그 책임은 전적으로 사용자에게 있습니다.\n\n이를 이해하고 동의하십니까?\n\n이 창은 최초 동의 후 나타나지 않습니다.");
@@ -66,15 +72,20 @@ import { getConfig, setConfig } from './config.mjs';
         if (document.getElementsByClassName("btn-login")[0]) clearInterval(afreecaUp);
         if(!document.getElementsByTagName("body")[0].className.includes("mode")) return;
         let bj = location.href.split("/")[3];
-        if (!upBj.includes(bj)) clearInterval(afreecaUp);
-        let dom = document.getElementById("like");
-        if (dom.className.includes('on')) {
+        console.log(`bj: ${bj}, upBj: ${upBj}`)
+        if (!upBj.includes(bj)) {
             clearInterval(afreecaUp);
         } else {
-            setTimeout(()=>{
-                dom.click();
-            },2000);
+            let dom = document.getElementById("like");
+            if (dom.className.includes('on')) {
+                clearInterval(afreecaUp);
+            } else {
+                setTimeout(()=>{
+                    dom.click();
+                },2000);
+            }
         }
+
     }, 5000);
 
     // 자정 넘어가면 자동 UP
@@ -88,15 +99,19 @@ import { getConfig, setConfig } from './config.mjs';
         let afreecaUp = setInterval(() => {
             if (document.getElementsByClassName("btn-login")[0]) clearInterval(afreecaUp);
             let bj = location.href.split("/")[3];
-            if (!upBj.includes(bj)) clearInterval(afreecaUp);
-            let dom = document.getElementById("like");
-            if (dom.className.includes('on')) {
-                clearInterval(afreecaUp);
+            if(!upBj.includes(bj)) {
+                clearInterval(afreecaUp)
             } else {
-                setTimeout(()=>{
-                    dom.click();
-                },2000);
+                let dom = document.getElementById("like");
+                if (dom.className.includes('on')) {
+                    clearInterval(afreecaUp);
+                } else {
+                    setTimeout(()=>{
+                        dom.click();
+                    },2000);
+                }
             }
+
         }, 1000);
     }, (86400-time)*1000+2000);
     
--- src/vodFuncs.js
+++ src/vodFuncs.js
@@ -136,7 +136,7 @@ import { getConfig, setConfig } from './config.mjs';
         //     });
         let path = await getConfig("twitch.path");
         console.log(path);
-        let ffmpegCommand = `ffmpeg -i "${m3u8Url}" -c copy "${path}${datetime()}.mp4"`
+        let ffmpegCommand = `ffmpeg -i "${m3u8Url}" -c copy "${path}${datetime()}.mp4"`;
         if (!checkLaw()) return;
         copyToClipboardOne(ffmpegCommand);
         document.getElementById("INGDLC-DL-IMG").style.filter = "";
