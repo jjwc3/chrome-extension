6dd3693faf2b8ab63fc99cb754918562b5762de5
jjwc3 <155734556+jjwc3@users.noreply.github.com>
2025-01-28 20:26:32 +0900

    250128 카페 게시판 제외 기능

---
+++ src/cafeExceptList.js
@@ -0,0 +1,38 @@
+import { getConfig } from './config.mjs';
+
+/*
+카페 전체글 보기에서 특정 게시판 제외
+ */
+(async () => {
+    let exceptList = (await getConfig("cafe.exceptList.list")).trim();
+    if (exceptList === "") return;
+
+    const mainFunc = async () => {
+        try {
+            if (!window.location.href.includes("ArticleList.nhn?search.clubid=29844827&search.boardtype=L")) return;
+            const articleList = document.getElementById("cafe_main").contentDocument.querySelectorAll(".td_article");
+            articleList.forEach((t) => {
+                try {
+                    const listName = t?.querySelector(".board-name")?.querySelector(".inner_name")?.firstElementChild?.innerHTML;
+                    if (exceptList.includes(listName)) {
+                        t.parentElement.style.display = "none";
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const observer = new MutationObserver(() => {
+        if (!(document.title === "Cafe")) mainFunc();
+    });
+    observer.observe(document.querySelector("title"), {
+        childList: true,
+        subtree: true,
+    });
+
+    await mainFunc();
+})();
